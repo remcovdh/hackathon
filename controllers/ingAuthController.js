6df54b3f09c1bae46ffe93eb9a41fa711b56b324
leonvandenoord <leon@vandenoord.org>
2014-11-13 00:16:45 +0100

    dfasfs

--- controllers/ingAuthController.js
+++ controllers/ingAuthController.js
@@ -3,16 +3,23 @@
  * A fully new page page.
  */
    var url= require('url');
-
+                       var User = require('../models/User');
 
 exports.index = function(req, res) {
-console.log(req);
-console.log(res);
-var url_parts = url.parse(req.url, true);
-console.log(req.url);
-var query = url_parts.hash;
-console.log(query);
 
+var url_parts = url.parse(req.url, true);
+
+var query = url_parts.query;
+var token = query["access_token"];
+console.log(token);
+                   console.log(req.user._id);
+     User.findById(req.user._id, function(err, user) {
+           user.tokens.push({ kind: 'ing', accessToken: token });
+           user.save(function(err) {
+            res.redirect('/');
+            // done(err, user);
+           });
+         });
               ;
 
 };
