b4791dcabcf31a0fa6445c52ca47bf3bfa1d964f
leonvandenoord <leon@vandenoord.org>
2014-11-13 10:19:31 +0100

    dfasfs

--- controllers/home.js
+++ controllers/home.js
@@ -13,26 +13,39 @@ var facebookProfile = null;
 exports.index = function(req, res) {
        response = res;
   User.findById(req.user.id, function(err, user) {
-   //   if (err) return next(err);
 
-
-   //   user.tokens = _.reject(user.tokens, function(token) { return token.kind === provider; });
-        //      console.log(user.tokens);
               var length = user.tokens.length;
+              var found = false;
               for(var i = 0;i<length;i++){
-        //      console.log(user.tokens[i].kind)
-                if(user.tokens[i].kind=='facebook'){
-            //    console.log(user.tokens[i].access_token);
-                                 graph
-                                   .setAccessToken(user.tokens[i].accessToken)
-                                   .get("/me", function(err, data) {
-                                   facebookProfile = data;
-                                   complete();
-                                   //    console.log(data);
-                                   });
+                   if(user.tokens[i].kind=='facebook'){
+                        found = true;
+                        graph
+                           .setAccessToken(user.tokens[i].accessToken)
+                           .get("/me", function(err, data) {
+                                              console.log(err);
+                           facebookProfile = data;
+                           complete();
+                           //    console.log(data);
+                           });
 
-                }
+                                /*   graph
+                                      .setAccessToken(user.tokens[i].accessToken)
+                                      .get("/me/photos", function(err, data) {
+
+                                         console.log(data);
+                                      }); */
+
+                    }
+
               }
+
+             if(!found){
+              response.render('home', {
+                  title: 'Home',
+                  fbProfile:{}
+
+                });
+              };
 
     });
 
@@ -42,6 +55,8 @@ exports.index = function(req, res) {
 function complete(){
   if(facebookProfile!==null){
     console.log(facebookProfile);
+    console.log(facebookProfile.location);
+    console.log(facebookProfile.location.name);
     response.render('home', {
         title: 'Home',
         fbProfile:facebookProfile
